2895ed9d8dc7587833be67c425165fa935bd21e5
Waqasbuttb1 <waqasansarb1@gmail.com>
2024-02-15 16:33:43 +0500

    n

--- headfootjav.js
+++ headfootjav.js
@@ -1,9 +1,23 @@
-// Fetch and insert header
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('header-placeholder').innerHTML = data);
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch and insert header
+    fetch('header.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error fetching header: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(data => document.getElementById('header-placeholder').innerHTML = data)
+        .catch(error => console.error(error));
 
-// Fetch and insert footer
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('footer-placeholder').innerHTML = data);
+    // Fetch and insert footer
+    fetch('footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error fetching footer: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(data => document.getElementById('footer-placeholder').innerHTML = data)
+        .catch(error => console.error(error));
+});
